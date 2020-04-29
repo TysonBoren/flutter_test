68a6a03b1e6d11e445ed47f32f4386ebe40e5c99
TysonBoren <tyson.d.boren@gmail.com>
2020-04-29 11:11:58 -0600

    added buttons and connected them to a basic function

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   //override is a 'decorator'. when you override a statless widget, its best practise to use @override for clarity.
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Test App'),
-        ), //AppBar
-        body: Text('default text'),
-      ), //Scaffold
-    ); //MaterialApp
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //additional code this is for the example of making this an object to have a larger function body
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
