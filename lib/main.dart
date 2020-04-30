06c290d2ba43c2d48b0e6a54614d0faa43c94aef
TysonBoren <tyson.d.boren@gmail.com>
2020-04-30 17:37:13 -0600

    created quiz and result files to manage the questions and ending result of completing the quiz. Moved the logic into those files and still works.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 // //main function is ran when the app launches
 // void main() {
@@ -20,7 +20,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('More questions ahead!');
     }
   }
@@ -56,23 +56,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Test App'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  //can use .elementAt or use [] to target by index.
-                  // Text(questions.elementAt(0)),
-                  Question(
-                    questions[_questionIndex]['questionText'],
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text('You did it!!'),
-              ),
+            : Result(),
       ),
     );
   }
